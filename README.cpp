dd4d17c95dc4b3dd0e8cdfdc815fd38b9450cdf8
ZiggityZaza <55286104+ZiggityZaza@users.noreply.github.com>
2025-01-24 13:58:32 +0100

    Update and rename README.md to README.cpp

---
+++ README.cpp
@@ -0,0 +1,3 @@
+int main() {
+  std::cout << "This would probably be written in C++ as it's the most portable language I know";
+}
--- README.md
+++
@@ -1,2 +0,0 @@
-# Tell-Me-Where
-Ollama to find a specific file on yo filesystem based on it's content.
